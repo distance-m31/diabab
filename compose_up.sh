0262f799dabcf7ab733c900f5bb7f5c650c8e03e
jpronkko <jukka.ronkko@iki.fi>
2024-06-06 15:57:14 +0300

    API url change

--- compose_up.sh
+++
@@ -1,5 +0,0 @@
-# Some commands
-docker compose up -f docker-compose.yml --build
-
-az webapp create --resource-group DiabResGroup --plan DiabAppServicePlan --name diabapp --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
-az webapp config container set --resource-group DiabResGroup --name diabapp --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
